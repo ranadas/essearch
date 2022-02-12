f8131d3e717433b13f86dbd60fc8896390c2d6b0
ranadas <rana.pratap.das@gmail.com>
2022-02-12 22:26:26 +0000

    Add keyword annotation to model

--- src/main/kotlin/com/rdas/es/model/Film.kt
+++ src/main/kotlin/com/rdas/es/model/Film.kt
@@ -4,29 +4,34 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.data.annotation.Id
 import org.springframework.data.elasticsearch.annotations.Document
 import org.springframework.data.elasticsearch.annotations.Field
+import org.springframework.data.elasticsearch.annotations.FieldType
 
 @Document(indexName = "movie-index")
 data class Film (
     @Id
     @JsonProperty("imdb_title_id")
-    @Field("imdb_title_id")
+    @Field(name = "imdb_title_id", type = FieldType.Keyword)
     val id: String,
-//    val imdb_title_id: String,
     val title: String,
     @JsonProperty("original_title")
-    @Field("original_title")
+    @Field(name = "original_title", type = FieldType.Keyword)
     val originalTitle: String,
     val year : Int= 0,
     val genre: String,
     val duration: Int,
+    @Field(name = "country", type = FieldType.Keyword)
     val country: String,
     val language: String,
+    @Field(name = "director", type = FieldType.Keyword)
     val director: String,
+    @Field(name = "writer", type = FieldType.Keyword)
     val writer: String,
     @JsonProperty("production_company")
     @Field("production_company")
     val productionCompany: String,
+    @Field(name = "actors", type = FieldType.Keyword)
     val actors: String,
+    @Field(name = "description", type = FieldType.Keyword)
     val description: String,
     val avg_vote : Double,
     val votes: Int,
